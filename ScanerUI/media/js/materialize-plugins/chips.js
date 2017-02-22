3ef7509bde30fee3174639cc2e244838cca1aff3
range <1510667109@qq.com>
2017-02-23 00:36:24 +0800

    Whole gourdscan refactoring

--- ScanerUI/media/js/materialize-plugins/chips.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-  $(document).ready(function() {
-
-    $(document).on('click.chip', '.chip .material-icons', function (e) {
-      $(this).parent().remove();
-    });
-
-  });
-}( jQuery ));
